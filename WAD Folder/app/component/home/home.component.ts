4a89676b2a2158c7a90c0ebd5e094ffac214c03a
VedikaGaikwad2004 <145186132+VedikaGaikwad2004@users.noreply.github.com>
2025-04-04 21:08:16 +0530

    Add files via upload

---
+++ WAD Folder/app/component/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  user: { email: string; password: string } = { email: '', password: '' };
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Retrieve user details from localStorage
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.user = JSON.parse(storedUser);
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('isLoggedIn');
+    this.router.navigate(['/login']);
+  }
+}
+
